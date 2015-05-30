6581d7775068681fee13f78d1d4ccbb42fabd90f
James Kieley <jkieley@asu.edu>
2015-05-29 21:53:42 -0700

    got the console game started

---
+++ Projects/ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********************************************************************");
+            Console.WriteLine(" _   _                 _               ");
+            Console.WriteLine("| \\ | |_   _ _ __ ___ | |__   ___ _ __ ");
+            Console.WriteLine("|  \\| | | | | '_ ` _ \\| '_ \\ / _ \\ '__|");
+            Console.WriteLine("| |\\  | |_| | | | | | | |_) |  __/ |   ");
+            Console.WriteLine("|_| \\_|\\__,_|_| |_| |_|_.__/ \\___|_|   ");
+            Console.WriteLine("                                       ");
+            Console.WriteLine("  ____                     _                ____                      ");
+            Console.WriteLine(" / ___|_   _  ___  ___ ___(_)_ __   __ _   / ___| __ _ _ __ ___   ___ ");
+            Console.WriteLine("| |  _| | | |/ _ \\/ __/ __| | '_ \\ / _` | | |  _ / _` | '_ ` _ \\ / _ \\");
+            Console.WriteLine("| |_| | |_| |  __/\\__ \\__ \\ | | | | (_| | | |_| | (_| | | | | | |  __/");
+            Console.WriteLine(" \\____|\\__,_|\\___||___/___/_|_| |_|\\__, |  \\____|\\__,_|_| |_| |_|\\___|");
+            Console.WriteLine("                                   |___/                              ");
+            Console.WriteLine("**********************************************************************");
+            Console.ReadLine();
+        }
+    }
+}
