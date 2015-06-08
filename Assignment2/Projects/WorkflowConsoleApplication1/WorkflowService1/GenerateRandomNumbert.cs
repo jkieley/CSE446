a6c93531b9c92f67bac70737f4663b4b794f0256
James Kieley <jkieley@asu.edu>
2015-06-07 23:23:35 -0700

    prepare assignment 2 for submission

---
+++ Assignment2/Projects/WorkflowConsoleApplication1/WorkflowService1/GenerateRandomNumbert.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+
+namespace WorkflowService1
+{
+
+    public sealed class GenerateRandomNumbert : CodeActivity
+    {
+        // Define an activity input argument of type string
+        public InArgument<int> lowerLimit { get; set; }
+        public InArgument<int> upperLimit { get; set; }
+        public OutArgument<int> result { get; set; }
+
+        // If your activity returns a value, derive from CodeActivity<TResult>
+        // and return the value from the Execute method.
+        protected override void Execute(CodeActivityContext context)
+        {
+            // Obtain the runtime value of the Text input argument
+
+            int lower = context.GetValue(this.lowerLimit);
+            int upper = context.GetValue(this.upperLimit);
+
+            DateTime currentDate = DateTime.Now;
+            int seed = (int)currentDate.Ticks;
+            Random random = new Random(seed);
+            int sNumber = random.Next(lower, upper);
+
+            result.Set(context, sNumber);
+        }
+    }
+}
---
+++ Assignment2/README.txt
@@ -0,0 +1,11 @@
+1. Workflow Foundation SOAP Service
+\Projects\WorkflowConsoleApplication1\WorkflowService1
+
+2. Workflow Application using at least 2 services
+\Projects\WorkflowConsoleApplication1\WorkflowConsoleApplication1
+
+3. Sensible Code Activity
+\Projects\WorkflowConsoleApplication1\WorkflowConsoleApplication1\TestGuessInput.cs
+
+4. User Manual
+\Assignment 2 User Manual.pdf
