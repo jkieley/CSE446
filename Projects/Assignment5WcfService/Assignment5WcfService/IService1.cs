70abe76430b79f305bbea5ecbe39e1c07a0a6ba3
James Kieley <jkieley@asu.edu>
2015-07-03 15:38:27 -0700

    sendMessage done, needs to be tested

---
+++ Projects/Assignment5WcfService/Assignment5WcfService/IService1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml.Linq;
+
+
+namespace Assignment5WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        void sendMessage(string senderId, string receiverId, string message);
+
+        [OperationContract]
+        List<Message> receiveMsg(string receiverId);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Message
+    {
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string Message { get; set; }
+        public XElement toElement() {
+            return new XElement("Message",
+                new XAttribute("SenderId", this.SenderId),
+                new XAttribute("ReceiverId", this.ReceiverId),
+                this.Message);
+        }
+    }
+}
+
---
+++ Projects/Assignment5WcfService/Assignment5WcfService/Service1.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Assignment5WcfService
+{
+    public class Service1 : IService1
+    {
+        string dbFileName = "~/App_Data/XMLMessagesDb.xml";
+        void sendMessage(string senderId, string receiverId, string message) {
+            XElement messages = XElement.Load(dbFileName);
+            messages.Add(new Message { SenderId = senderId, ReceiverId = receiverId, Message = message }.toElement());
+            messages.Save(dbFileName);
+        }
+
+        List<Message> receiveMsg(string receiverId) {
+            return null;
+        }
+    }
+}
